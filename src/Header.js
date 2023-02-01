ffdb5a81682f0b94094f747b8b7cd1394129b467
steven gant <sgant39@gmail.com>
2023-02-01 23:22:46 +0000

    added styling

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,10 @@ import { withAuth0 } from "@auth0/auth0-react";
 class Header extends React.Component {
   render() {
     return (
+      <>
+      <h3 className='metal linear'>CONCERT-GOGO</h3>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Devs</Navbar.Brand>
+        {/* <Navbar.Brand>CONCERT-GOGO</Navbar.Brand> */}
         <NavItem><Link to="/About" className="nav-link">About</Link></NavItem>
         <NavItem><Link to="/Search" className="nav-link">Search</Link></NavItem>
         <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem>
@@ -24,6 +26,7 @@ class Header extends React.Component {
 
         {/* PLACEHOLDER: render a navigation link to the about page */}
       </Navbar>
+      </>
     );
   }
 }
